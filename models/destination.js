f1fe353f34da5430f71850a4cfa1c86d1e3dd173
TRah <tbello58@gmail.com>
2020-07-26 17:24:39 -0600

    proper documentation

--- models/destination.js
+++ models/destination.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-// Step 1: Define our Schema
-// See: https://mongoosejs.com/docs/guide.html
 /*
 "Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection."
 */
@@ -21,10 +19,8 @@ const gallerySchema = new mongoose.Schema(
   }
 );
 
-// Compile and export our model using the above Schema.
-// See: https://mongoosejs.com/docs/models.html 
-
+// Compile and export model using the above Schema.
 module.exports = mongoose.model('destination', gallerySchema);
-// Important: The first argument of mongoose.model() is the singular name of the collection your model is for. 
-// ** Mongoose automatically looks for the plural, lowercased version of your model name. **"
-// In our example, we name our model 'Definition' and mongoose will automatically look for the collection 'definitions' 
+
+// The first argument of mongoose.model() is the singular name of the collection the model is for. 
+// ** Mongoose automatically looks for the plural, lowercased version of the model name. **
